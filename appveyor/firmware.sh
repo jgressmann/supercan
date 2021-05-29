eb33612869d4c94f4f2a6a2477069c2eaeedf483
Jean Gressmann <jean@0x42.de>
2021-05-29 21:13:20 +0200

    Update firmware build script

--- appveyor/firmware.sh
+++ appveyor/firmware.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -e -x
 
 export DEBIAN_FRONTEND=noninteractive
 export BOARD=d5035-01
@@ -7,7 +7,6 @@ export PID_RT=0x5035
 export PID_DFU=0x5036
 export MAKE_ARGS="-j V=1"
 export BOOTLOADER_NAME="D5035-01 SuperCAN DFU"
-export BOOTLOADER_MAKE_ARGS="$MAKE_ARGS BOARD=$BOARD BOOTLOADER=1 VID=$VID PID=$PID_DFU \"PRODUCT_NAME=$BOOTLOADER_NAME\" \"INTERFACE_NAME=$BOOTLOADER_NAME\""
 export TARGET_DIR=$APPVEYOR_BUILD_FOLDER/tmp
 
 hw_revs="$(seq 3)"
@@ -19,18 +18,21 @@ sudo apt-get update && sudo apt-get install -y dfu-util gcc-arm-none-eabi
 git submodule update --init --recursive
 
 env
-mkdir -p $TARGET_DIR/supercan/$BOARD/01
-mkdir -p $TARGET_DIR/supercan/$BOARD/02
-mkdir -p $TARGET_DIR/supercan/$BOARD/03
 
 # Save current commit
 echo $APPVEYOR_REPO_COMMIT >$TARGET_DIR/supercan/COMMIT
+
+# output dirs
+for i in "$hw_revs"; do
+	mkdir -p $TARGET_DIR/supercan/$BOARD/0$i
+done
+
 
 # SuperDFU
 cd $APPVEYOR_BUILD_FOLDER/Boards/examples/device/atsame51_dfu
 
 for i in "$hw_revs"; do
-	make $BOOTLOADER_MAKE_ARGS HWREV=$i
+	make $MAKE_ARGS BOARD=$BOARD BOOTLOADER=1 VID=$VID PID=$PID_DFU "PRODUCT_NAME=$BOOTLOADER_NAME" "INTERFACE_NAME=$BOOTLOADER_NAME" HWREV=$i
 	cp _build/build-$BOARD/$BOARD-firmware.hex $TARGET_DIR/supercan/$BOARD/0$i/superdfu.hex
 	rm -rf _build
 done
