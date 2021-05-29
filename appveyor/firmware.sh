11ab3d9a8c38bb5b550da20bd52ffd6a23133903
Jean Gressmann <jean@0x42.de>
2021-05-29 21:03:54 +0200

    Adds initial version of firmware build script for appveyor

---
+++ appveyor/firmware.sh
@@ -0,0 +1,54 @@
+#!/bin/sh -e
+
+export DEBIAN_FRONTEND=noninteractive
+export BOARD=d5035-01
+export VID=0x1d50
+export PID_RT=0x5035
+export PID_DFU=0x5036
+export MAKE_ARGS="-j V=1"
+export BOOTLOADER_NAME="D5035-01 SuperCAN DFU"
+export BOOTLOADER_MAKE_ARGS="$MAKE_ARGS BOARD=$BOARD BOOTLOADER=1 VID=$VID PID=$PID_DFU \"PRODUCT_NAME=$BOOTLOADER_NAME\" \"INTERFACE_NAME=$BOOTLOADER_NAME\""
+export TARGET_DIR=$APPVEYOR_BUILD_FOLDER/tmp
+
+hw_revs="$(seq 3)"
+
+# install build dependencies
+sudo apt-get update && sudo apt-get install -y dfu-util gcc-arm-none-eabi
+
+# init submodules
+git submodule update --init --recursive
+
+env
+mkdir -p $TARGET_DIR/supercan/$BOARD/01
+mkdir -p $TARGET_DIR/supercan/$BOARD/02
+mkdir -p $TARGET_DIR/supercan/$BOARD/03
+
+# Save current commit
+echo $APPVEYOR_REPO_COMMIT >$TARGET_DIR/supercan/COMMIT
+
+# SuperDFU
+cd $APPVEYOR_BUILD_FOLDER/Boards/examples/device/atsame51_dfu
+
+for i in "$hw_revs"; do
+	make $BOOTLOADER_MAKE_ARGS HWREV=$i
+	cp _build/build-$BOARD/$BOARD-firmware.hex $TARGET_DIR/supercan/$BOARD/0$i/superdfu.hex
+	rm -rf _build
+done
+
+# SuperCAN
+cd $APPVEYOR_BUILD_FOLDER/Boards/examples/device/supercan
+
+for i in "$hw_revs"; do
+	make $MAKE_ARGS HWREV=$i
+	cp _build/build-$BOARD/$BOARD-firmware.hex $TARGET_DIR/supercan/$BOARD/0$i/supercan-standalone.hex
+	rm -rf _build
+	make $MAKE_ARGS HWREV=$i APP=1 dfu
+	cp _build/build-$BOARD/$BOARD-firmware.superdfu.hex $TARGET_DIR/supercan/$BOARD/0$i/supercan-dfu.hex
+	cp _build/build-$BOARD/$BOARD-firmware.dfu $TARGET_DIR/supercan/$BOARD/0$i/supercan.dfu
+	rm -rf _build
+done
+
+# archive
+cd $TARGET_DIR && tar cfz supercan-firmware.tar.gz supercan
+
+# find $TARGET_DIR || true
