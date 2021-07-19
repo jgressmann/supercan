0229e4724aeabbc4fa54d32bbdbd56efcec49eca
Jean Gressmann <jean@0x42.de>
2021-07-19 20:56:51 +0200

    Windows build debugging

--- appveyor/windows.cmd
+++ appveyor/windows.cmd
@@ -9,7 +9,6 @@ REM Qt Build
 mkdir qt\32
 cd qt\32
 (call "%VCVARS32%" x86 && "%QT_BASE_DIR32%\bin\qmake.exe" ..\..\Windows\qtsupercanbus\supercan.pro -spec win32-msvc "CONFIG+=qtquickcompiler" && nmake qmake_all && nmake) || exit /b !ERRORLEVEL!
-REM (  && nmake qmake_all) || exit /b !ERRORLEVEL!
 cd ..\..
 mkdir qt\64
 cd qt\64
@@ -33,7 +32,7 @@ xcopy /y /f Windows\Win32\Release\supercan_srv32.exe bin\x64\
 xcopy /y /f Windows\x64\Release\supercan_srv64.exe bin\x64\
 xcopy /y /f qt\32\plugins\canbus\*.dll bin\x86\
 xcopy /y /f qt\64\plugins\canbus\*.dll bin\x64\
-7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on supercan-win.7z bin lib inc src || exit /b !ERRORLEVEL!
+(7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on supercan-win.7z bin lib inc src) || exit /b !ERRORLEVEL!
 REM installer
 (call "%VCVARS64%" x64 && makensis /DSC_VERSION_MAJOR=%PRODUCT_VERSION_MAJOR% /DSC_VERSION_MINOR=%PRODUCT_VERSION_MINOR% /DSC_VERSION_PATCH=%PRODUCT_VERSION_PATCH% /DSC_VERSION_BUILD=%PRODUCT_VERSION_BUILD% Windows\NSIS\supercan.nsi) || exit /b !ERRORLEVEL!
 move Windows\NSIS\supercan_inst.exe .
