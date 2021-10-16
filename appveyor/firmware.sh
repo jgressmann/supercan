068b2a71816b671f874f2465f3c259f2fb8c945a
Jean Gressmann <jean@0x42.de>
2021-10-16 14:40:38 +0200

    Update board code, firmware build script

--- appveyor/firmware.sh
+++ appveyor/firmware.sh
@@ -3,8 +3,13 @@
 set -e
 set -x
 
+if [ -z "$APPVEYOR_BUILD_FOLDER" ]; then
+	echo "ERROR: APPVEYOR_BUILD_FOLDER not set"
+	exit 1
+fi
+
 export DEBIAN_FRONTEND=noninteractive
-export BOARD=d5035-01
+
 export VID=0x1d50
 export PID_RT=0x5035
 export PID_DFU=0x5036
@@ -12,7 +17,7 @@ export MAKE_ARGS="-j V=1"
 export BOOTLOADER_NAME="D5035-01 SuperCAN DFU"
 export TARGET_DIR=$APPVEYOR_BUILD_FOLDER/tmp
 
-hw_revs=3
+
 
 # install build dependencies
 sudo apt-get update && sudo apt-get install -y dfu-util gcc-arm-none-eabi
@@ -26,37 +31,69 @@ env
 mkdir -p $TARGET_DIR/supercan
 echo $APPVEYOR_REPO_COMMIT >$TARGET_DIR/supercan/COMMIT
 
+############
+# D5035-01 #
+############
+hw_revs=3
+export BOARD=d5035_01
+
 # make output dirs for hw revs
 for i in $hw_revs; do
 	mkdir -p $TARGET_DIR/supercan/$BOARD/0$i
 done
 
 
+
 # SuperDFU
-cd $APPVEYOR_BUILD_FOLDER/Boards/examples/device/atsame51_dfu
+project=atsame51_dfu
+cd $APPVEYOR_BUILD_FOLDER/Boards/examples/device/${project}
+
 
 for i in $hw_revs; do
 	make $MAKE_ARGS BOARD=$BOARD BOOTLOADER=1 VID=$VID PID=$PID_DFU PRODUCT_NAME="$BOOTLOADER_NAME" INTERFACE_NAME="$BOOTLOADER_NAME" HWREV=$i
-	cp _build/build-$BOARD/$BOARD-firmware.hex $TARGET_DIR/supercan/$BOARD/0$i/superdfu.hex
-	cp _build/build-$BOARD/$BOARD-firmware.bin $TARGET_DIR/supercan/$BOARD/0$i/superdfu.bin
+	cp _build/$BOARD/${project}.hex $TARGET_DIR/supercan/$BOARD/0$i/superdfu.hex
+	cp _build/$BOARD/${project}.bin $TARGET_DIR/supercan/$BOARD/0$i/superdfu.bin
 	rm -rf _build
 	make $MAKE_ARGS BOARD=$BOARD BOOTLOADER=1 VID=$VID PID=$PID_DFU PRODUCT_NAME="$BOOTLOADER_NAME" INTERFACE_NAME="$BOOTLOADER_NAME" HWREV=$i APP=1 dfu
-	cp _build/build-$BOARD/$BOARD-firmware.dfu $TARGET_DIR/supercan/$BOARD/0$i/superdfu.dfu
+	cp _build/$BOARD/${project}.dfu $TARGET_DIR/supercan/$BOARD/0$i/superdfu.dfu
 	rm -rf _build
 done
 
 # SuperCAN
-cd $APPVEYOR_BUILD_FOLDER/Boards/examples/device/supercan
+project=supercan
+cd $APPVEYOR_BUILD_FOLDER/Boards/examples/device/${project}
+
 
 for i in $hw_revs; do
 	make $MAKE_ARGS HWREV=$i
-	cp _build/build-$BOARD/$BOARD-firmware.hex $TARGET_DIR/supercan/$BOARD/0$i/supercan-standalone.hex
-	cp _build/build-$BOARD/$BOARD-firmware.bin $TARGET_DIR/supercan/$BOARD/0$i/supercan-standalone.bin
+	cp _build/$BOARD/${project}.hex $TARGET_DIR/supercan/$BOARD/0$i/supercan-standalone.hex
+	cp _build/$BOARD/${project}.bin $TARGET_DIR/supercan/$BOARD/0$i/supercan-standalone.bin
 	rm -rf _build
 	make $MAKE_ARGS HWREV=$i APP=1 dfu
-	cp _build/build-$BOARD/$BOARD-firmware.superdfu.hex $TARGET_DIR/supercan/$BOARD/0$i/supercan-dfu.hex
-	cp _build/build-$BOARD/$BOARD-firmware.superdfu.bin $TARGET_DIR/supercan/$BOARD/0$i/supercan-dfu.bin
-	cp _build/build-$BOARD/$BOARD-firmware.dfu $TARGET_DIR/supercan/$BOARD/0$i/supercan.dfu
+	cp _build/$BOARD/${project}.superdfu.hex $TARGET_DIR/supercan/$BOARD/0$i/supercan-dfu.hex
+	cp _build/$BOARD/${project}.superdfu.bin $TARGET_DIR/supercan/$BOARD/0$i/supercan-dfu.bin
+	cp _build/$BOARD/${project}.dfu $TARGET_DIR/supercan/$BOARD/0$i/supercan.dfu
+	rm -rf _build
+done
+unset hw_revs
+
+#####################
+# Teensy 4.x boards #
+#####################
+
+
+
+
+boards="teensy_40 d5035_03"
+for board in $boards; do
+	export BOARD=$board
+
+	mkdir -p $TARGET_DIR/supercan/$BOARD
+
+	make $MAKE_ARGS
+
+	cp _build/$BOARD/${project}.hex $TARGET_DIR/supercan/$BOARD/supercan.hex
+	cp _build/$BOARD/${project}.bin $TARGET_DIR/supercan/$BOARD/supercan.bin
 	rm -rf _build
 done
 
