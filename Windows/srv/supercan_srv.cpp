cd65786549eb513a30b28abea150f1bfc6eb8d44
Jean Gressmann <jean@0x42.de>
2022-04-02 20:54:06 +0200

    Windows: COM server: remove unused command line parsing code

--- Windows/srv/supercan_srv.cpp
+++ Windows/srv/supercan_srv.cpp
@@ -42,42 +42,16 @@ public :
 
 
 CSuperCANSrvModule _AtlModule;
-int g_ThreadPriority = THREAD_PRIORITY_NORMAL;
-DWORD g_PriorityClass = NORMAL_PRIORITY_CLASS;
 
 
 
 //
 extern "C" int WINAPI _tWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/,
-								LPTSTR lpCmdLine, int nShowCmd)
+								LPTSTR /*lpCmdLine*/, int nShowCmd)
 {
-	int argc = 0;
-	LPWSTR* argv = CommandLineToArgvW(lpCmdLine, &argc);
-	int rc = 0;
-
-	if (argv) {
-		DWORD priority_class = NORMAL_PRIORITY_CLASS;
-
-		for (int i = 0; i < argc; ++i) {
-			LPWSTR arg = argv[i];
-			if (arg[0] == L'/' || arg[0] == L'-') {
-				if (_wcsicmp(L"HighPriority", &arg[1]) == 0) {
-					g_PriorityClass = HIGH_PRIORITY_CLASS;
-				}
-				else if (_wcsicmp(L"AboveNormalPriority", &arg[1]) == 0) {
-					g_PriorityClass = ABOVE_NORMAL_PRIORITY_CLASS;
-				}
-			}
-		}
-
-		LocalFree(argv);
-
-		SetPriorityClass(GetCurrentProcess(), priority_class);
-	}
-
 	LOG_SRV(SC_DLL_LOG_LEVEL_DEBUG, "WinMain start\n");
 
-	rc = _AtlModule.WinMain(nShowCmd);
+	auto rc = _AtlModule.WinMain(nShowCmd);
 
 	LOG_SRV(SC_DLL_LOG_LEVEL_DEBUG, "WinMain end\n");
 
