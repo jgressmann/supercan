0001bce810f1e3465b62553c8db042603753d38f
Jean Gressmann <jean@0x42.de>
2022-06-04 09:01:31 +0200

    Adds documentation to firmware archive
    - toc
    - firmware update / install instructions

--- appveyor/firmware.sh
+++ appveyor/firmware.sh
@@ -57,6 +57,30 @@ for i in $hw_revs; do
 	make $MAKE_ARGS BOARD=$BOARD BOOTLOADER=1 VID=$VID PID=$PID_DFU PRODUCT_NAME="$BOOTLOADER_NAME" INTERFACE_NAME="$BOOTLOADER_NAME" HWREV=$i APP=1 dfu
 	cp _build/$BOARD/${project}.dfu $TARGET_DIR/supercan/$BOARD/0$i/superdfu.dfu
 	rm -rf _build
+
+	# generate J-Link flash script
+	cat >$TARGET_DIR/supercan/$BOARD/0$i/superdfu.jlink <<EOF
+r
+loadfile superdfu.hex
+r
+go
+exit
+EOF
+
+	# generate README
+	cat >>$TARGET_DIR/supercan/$BOARD/0$i/README.md <<EOF
+# SuperCAN Device Firmware
+
+## Content
+### Device Bootloader (SuperDFU)
+
+- superdfu.bin: binary, flash with debug probe
+- superdfu.hex: hex, flash with debug probe
+- superdfu.dfu: update with dfu-util
+- superdfu.jlink: J-Link flash script
+
+EOF
+
 done
 
 # SuperCAN
@@ -74,15 +98,111 @@ for i in $hw_revs; do
 	cp _build/$BOARD/${project}.superdfu.bin $TARGET_DIR/supercan/$BOARD/0$i/supercan-dfu.bin
 	cp _build/$BOARD/${project}.dfu $TARGET_DIR/supercan/$BOARD/0$i/supercan.dfu
 	rm -rf _build
+
+	# generate J-Link flash script (standalone)
+	cat >$TARGET_DIR/supercan/$BOARD/0$i/superdfu-standalone.jlink <<EOF
+r
+loadfile supercan-standalone.hex
+r
+go
+exit
+EOF
+
+	# generate J-Link flash script (requires bootloader)
+	cat >$TARGET_DIR/supercan/$BOARD/0$i/superdfu-dfu.jlink <<EOF
+r
+loadfile supercan-dfu.hex
+r
+go
+exit
+EOF
+
+
+	cat >>$TARGET_DIR/supercan/$BOARD/0$i/README <<EOF
+### CAN Application (SuperCAN)
+
+- supercan-standalone.bin: binary, no bootloader required, flash with debug probe
+- supercan-standalone.hex: hex, no bootloader required, flash with debug probe
+- supercan-standalone.jlink: J-Link flash script
+- supercan-dfu.bin: binary, requires bootloader, flash with debug probe to 0x4000
+- supercan-dfu.hex: hex, requires bootloader, flash with debug probe to 0x4000
+- supercan-dfu.jlink: J-Link flash script
+- supercan.dfu: requires bootloader, update with dfu-util
+
+
+## Installation / Upgrade
+
+If you are using the bootloader, update it *first*.
+After the bootloader update, you likely need to re-install the CAN application.
+
+### Device Bootloader
+
+#### J-LINK
+
+```
+JLinkExe -device ATSAME51J18 -if swd -JTAGConf -1,-1 -speed auto -CommandFile superdfu.jlink
+```
+
+
+### CAN Application (no Bootloder Required)
+
+#### J-LINK
+
+```
+JLinkExe -device ATSAME51J18 -if swd -JTAGConf -1,-1 -speed auto -CommandFile supercan-standalone.jlink
+```
+
+### CAN Application (Bootlodaer Required)
+
+#### J-LINK
+
+```
+JLinkExe -device ATSAME51J18 -if swd -JTAGConf -1,-1 -speed auto -CommandFile supercan-dfu.jlink
+```
+
+EOF
+
 done
 unset hw_revs
+
+########################
+# SAM E54 Xplained Pro #
+########################
+
+export BOARD=same54xplainedpro
+
+mkdir -p $TARGET_DIR/supercan/$BOARD
+
+make $MAKE_ARGS
+
+cp _build/$BOARD/${project}.hex $TARGET_DIR/supercan/$BOARD/
+cp _build/$BOARD/${project}.bin $TARGET_DIR/supercan/$BOARD/
+rm -rf _build
+
+# generate J-Link flash script (standalone)
+cat >$TARGET_DIR/supercan/$BOARD/README.md <<EOF
+# SuperCAN Device Firmware
+
+## Content
+- supercan.bin: binary, flash with debug probe
+- supercan.hex: hex, flash with debug probe
+
+## Installation
+### EDBG
+
+```
+edbg --verbose -t same54 -pv -f supercan.bin
+```
+
+EOF
+
 
 ##########################
 # Other Boards (bin/hex) #
 ##########################
 
 
-boards="teensy_40 d5035_03 same54xplainedpro"
+boards="teensy_40 d5035_03"
 for board in $boards; do
 	export BOARD=$board
 
